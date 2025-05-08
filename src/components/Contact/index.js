4a399ea69c2086b4858349c1da2b511c19699636
Divyani <divyanideshmukh8@gmail.com>
2025-05-08 18:05:03 +0530

    Initial commit with changes

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_tox7kqs', 'template_nv7k7mj', form.current, 'SybVGsYS52j2TfLbi')
+    emailjs.sendForm('service_xhqf88l', 'template_6z9dx3q', form.current, 'YBjAARlWkTm_LmpJI')
       .then((result) => {
         setOpen(true);
         form.current.reset();
